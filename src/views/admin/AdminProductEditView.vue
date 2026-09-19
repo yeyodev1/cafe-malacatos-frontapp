56a0b6e5ee17be82148b067da32cb36b757a00e4
Diego Reyes <diegorele13@gmail.com>
2026-09-19 14:59:45 -0500

    chore: placeholder de AdminProductEditView

---
+++ src/views/admin/AdminProductEditView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+// Placeholder para que el build pase: esta vista la construye la siguiente tanda.
+</script>
+
+<template>
+  <section class="placeholder">
+    <p>En construcción</p>
+  </section>
+</template>
